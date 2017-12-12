d42b31b66609de1bb75c14ba4bf8646046d6dcb0
tardisdriver <stark.tracy@gmail.com>
2017-12-11 23:44:29 -0600

    writing tests

--- src/components/service-check.test.js
+++ src/components/service-check.test.js
@@ -8,9 +8,15 @@ describe('<ServiceCheck />', () => {
         shallow(<ServiceCheck />)
     });
     it('should call api when number is submitted', () => {
-        const callback = ;
+        const callback = jest.fn();
         const wrapper = mount(<ServiceCheck onSubmit={callback} />);
         wrapper.simulate('submit');
         expect()
-    })
+    });
+    it('should take user to next page if service number exists', () => {
+
+    });
+    it('should return error message if number does not exist', () => {
+
+    });
 });
