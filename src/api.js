000a9089454cd72e0bfaaaaab50e003ca10b0759
tardisdriver <stark.tracy@gmail.com>
2018-01-05 10:33:40 -0600

    changed API addresses

--- src/api.js
+++ src/api.js
@@ -6,9 +6,17 @@
 
 //local point to drakov, on netlify, point to apiary
 
+let HOST;
+
+if (process.env.NODE_ENV === 'production') {
+    HOST = 'https://cryptic-sierra-54081.herokuapp.com';
+} else {
+    HOST = 'http://localhost:3000';
+}
+
 
 export function checkServiceNumber(number) {
-    return fetch(`http://localhost:3000/customers/${number}`)
+    return fetch(`${HOST}/customers/${number}`)
         .then((res) => {
             if (res.ok) {
                 return res.json()
@@ -16,5 +24,5 @@ export function checkServiceNumber(number) {
                 return false
             }
             throw { message: 'There was a problem with your request' }
-        })
+        });
 }
