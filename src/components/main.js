71d39aa6c050447109f2d70b495240d035a9d176
tardisdriver <stark.tracy@gmail.com>
2018-01-10 14:42:08 -0600

    added ChatBot container

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,15 @@
 import React from 'react';
-
+import ChatBot from '../containers/chat-bot';
 import ServiceCheck from '../containers/service-check';
 
 export default function (props) {
     if (props.customerFound) {
-        return <h1>Found</h1>
-    } else {
-
         return (
-            <div>
-                <ServiceCheck />
-            </div>
+            <ChatBot />
+        )
+    } else {
+        return (
+            <ServiceCheck />
         )
     }
 }
