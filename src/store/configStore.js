1a4433f2b31c97b469001c2d587769e90b58b723
tardisdriver <stark.tracy@gmail.com>
2018-01-24 11:02:15 -0600

    added redux web tool

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,9 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
-const configureStore = (preLoadedState) => createStore(rootReducer, preLoadedState, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const configureStore = (preLoadedState) => createStore(rootReducer, preLoadedState, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 
 
