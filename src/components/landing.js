38046236497cfa0ab6a8c13a43997bcee0dace9d
tardisdriver <stark.tracy@gmail.com>
2018-01-29 16:44:19 -0600

    non-mobile CSS updates

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import InfoBox from './infobox';
+import Button from './bot-button';
 import './landing.css';
 import shrubbot from './images/shrub-bot.png';
 
@@ -8,9 +9,6 @@ const boxes = [
     { text: 'Automated Support', body: 'Free up your support staff while SHRUB takes care of frequently asked questions.' },
     { text: 'FAQ Aware', body: 'SHRUB will digest your FAQ documents and deliver the answers to your clients in a familiar, text-like conversation.' },
 ];
-
-//write onClick function and then add onclick to go-to-bot button
-//onCLick function switches continueClicked from false to true
 
 export default function Landing(props) {
     const boxesRender = boxes.map((box, index) =>
@@ -23,11 +21,12 @@ export default function Landing(props) {
             <div className='info'>
                 {boxesRender}
             </div>
-            <button className='go-to-bot'>Click Here To Try Out Talking To SHRUB</button>
+            {/* <button className='go-to-bot' onClick={props.action}>Click Here To Try Out Talking To SHRUB</button> */}
+            <Button onClick={props.action} />
             <div className='about-text'>
                 <p>SHRUB is a chat bot that is intended to assist companies looking to free up their support staff to work on more complex issues while SHRUB takes care of simple, frequently asked questions from your client base. SHRUB works off of FAQ documents that you provide, and it delivers the answers to your clients.</p>
             </div>
-        </div>
+        </div >
 
     )
 }
