3c2b839a006386fa1827a85343cc63c6c17107bc
tardisdriver <stark.tracy@gmail.com>
2018-02-07 11:39:05 -0600

    improved demo instructions

--- src/components/service-check.js
+++ src/components/service-check.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import './service-check.css';
+import "./service-check.css";
 
 export default class ServiceCheck extends React.Component {
-    onSubmit(event) {
-        event.preventDefault();
-        const entry = this.textInput.value;
-        this.props.submitServiceNumber(entry)
-    }
+  onSubmit(event) {
+    event.preventDefault();
+    const entry = this.textInput.value;
+    this.props.submitServiceNumber(entry);
+  }
 
-
-    render() {
-        return (
-            <div>
-                <form id='service-check' onSubmit={e => this.onSubmit(e)}>
-                    <span id='svc-text'>Please enter your service number to continue</span>
-                    <input type='text' name='svc-entry' id='svc-entry' placeholder='ABC123' ref={input => (this.textInput = input)}></input>
-                    <button type='submit' className='svc-sub'>Submit</button>
-                    {this.props.invalidNumber ? <span className='error-message'>Sorry, could not find your number.  Please try again</span> : null}
-                    {this.props.error ? <span className='error-message'>{this.props.error}</span> : null}
-                </form >
-                <div className='tip-box'>
-                    <p>For a demo, use the service number 654ZYX.</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form id="service-check" onSubmit={e => this.onSubmit(e)}>
+          <span id="svc-text">
+            Please enter your service number to continue
+          </span>
+          <input
+            type="text"
+            name="svc-entry"
+            id="svc-entry"
+            placeholder="ABC123"
+            ref={input => (this.textInput = input)}
+          />
+          <button type="submit" className="svc-sub">
+            Submit
+          </button>
+          {this.props.invalidNumber ? (
+            <span className="error-message">
+              Sorry, could not find your number. Please try again
+            </span>
+          ) : null}
+          {this.props.error ? (
+            <span className="error-message">{this.props.error}</span>
+          ) : null}
+        </form>
+        <div className="tip-box">
+          <p>
+            For a demo, use the service number <b>654ZYX</b>.{" "}
+          </p>
+          <p>
+            This number is tied to auser account, and for this demo, you get to
+            be Bob Belcher!
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
