d3854b48053bdeff3fe06569500931132d544590
tardisdriver <stark.tracy@gmail.com>
2018-02-07 11:37:15 -0600

    auto cap service number when entered in lower case

--- src/api.js
+++ src/api.js
@@ -1,58 +1,57 @@
 let HOST;
 
-if (process.env.NODE_ENV === 'production') {
-    HOST = 'https://cryptic-sierra-54081.herokuapp.com/';
+if (process.env.NODE_ENV === "production") {
+  HOST = "https://cryptic-sierra-54081.herokuapp.com/";
 } else {
-    HOST = 'http://localhost:8080';
+  HOST = "http://localhost:8080";
 }
 
 export function checkServiceNumber(number) {
-    return fetch(`${HOST}/customers/${number}`)
-        .then((res) => {
-            if (res.ok) {
-                return res.json()
-            } else if (res.status === 404) {
-                return false
-            }
-            throw new Error('There was a problem with your request')
-        });
+  const capSvc = number.toUpperCase();
+  return fetch(`${HOST}/customers/${capSvc}`).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else if (res.status === 404) {
+      return false;
+    }
+    throw new Error("There was a problem with your request");
+  });
 }
 
 export function sendMessage(message, number, conversationID) {
-    const headers = new Headers({
-        "X-Service-Number": number.toString(),
-    });
-    return fetch(`${HOST}/conversations/${conversationID}`, {
-        method: 'POST',
-        body: message,
-        headers
-    }).then((res) => {
-        if (res.ok) {
-            return res.text()
-        } else if (res.status === 404) {
-            throw new Error('Conversation not found')
-        } else if (res.status === 403) {
-            throw new Error('Not authorized to view this conversation')
-        }
-        throw new Error('There was a problem with your request')
-    })
+  const headers = new Headers({
+    "X-Service-Number": number.toString()
+  });
+  return fetch(`${HOST}/conversations/${conversationID}`, {
+    method: "POST",
+    body: message,
+    headers
+  }).then(res => {
+    if (res.ok) {
+      return res.text();
+    } else if (res.status === 404) {
+      throw new Error("Conversation not found");
+    } else if (res.status === 403) {
+      throw new Error("Not authorized to view this conversation");
+    }
+    throw new Error("There was a problem with your request");
+  });
 }
 
 export function startConversation(number) {
-    const headers = new Headers({
-        "X-Service-Number": number.toString()
-    });
-    return fetch(`${HOST}/conversations`, {
-        method: 'GET',
-        mode: 'cors',
-        headers
-    }).then((res) => {
-        if (res.ok) {
-            return res.json()
-        } else if (res.status === 302) {
-            throw new Error('Not authorized to create conversations')
-        }
-        throw new Error('There was a problem with your request')
-    })
-
+  const headers = new Headers({
+    "X-Service-Number": number.toString()
+  });
+  return fetch(`${HOST}/conversations`, {
+    method: "GET",
+    mode: "cors",
+    headers
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else if (res.status === 302) {
+      throw new Error("Not authorized to create conversations");
+    }
+    throw new Error("There was a problem with your request");
+  });
 }
