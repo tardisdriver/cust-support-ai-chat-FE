dd9851a2784599ad33f71d5d3ea844a09f6ec9fd
tardisdriver <stark.tracy@gmail.com>
2018-02-21 13:28:21 -0600

    added more explaination to the service check page

--- src/components/service-check.js
+++ src/components/service-check.js
@@ -13,6 +13,16 @@ export default class ServiceCheck extends React.Component {
     return (
       <div>
         <form id="service-check" onSubmit={e => this.onSubmit(e)}>
+          <p class="service-info">
+            Here, SHRUB can identify your client by some ID that you've
+            provided, which is stored in a database. This allows you to add an
+            additional check that can do many things, such as check the client's
+            service contract for validity or issues, or even pull up the
+            client's known hardware configuration which can then be ingested by
+            SHRUB to give more targeted answers. In this demo, all the service
+            number does for the moment is allow SHRUB to know the name of your
+            client so she can greet them properly.
+          </p>
           <span id="svc-text">
             Please enter your service number to continue
           </span>
